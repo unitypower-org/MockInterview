bbf08a376cb74289895f310d698bf987e3a51d23
muhammad-abdulloh <clevercoderr@gmail.com>
2022-12-18 12:05:46 +0500

    DOMAIN: Create SelectedInterlocutorInterviewCategoriesDTO view Model

---
+++ MockInterview.Domain/Models/Interlocutor/SelectedInterlocutorInterviewCategoriesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterview.Domain.Models.Interlocutor
+{
+    public class SelectedInterlocutorInterviewCategoriesDTO
+    {
+        public int InterviewerId { get; set; }
+        public string InterviewTime { get; set; } = string.Empty;
+        public string InterviewPrice { get; set; } = string.Empty;
+        public string InterviewerProjects { get; set; } = string.Empty;
+        public int InterlocutorId { get; set; }
+    }
+}
