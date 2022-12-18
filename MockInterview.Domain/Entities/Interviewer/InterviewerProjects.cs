8ffce85a32f9f42680e86b04b50339fecb639f29
muhammad-abdulloh <clevercoderr@gmail.com>
2022-12-18 10:43:20 +0500

    DOMAIN: Create InterviewerProjects Model

---
+++ MockInterview.Domain/Entities/Interviewer/InterviewerProjects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterview.Domain.Entities.Interviewer
+{
+    public class InterviewerProjects
+    {
+        public int Id { get; set; }
+        public int InterviewerId { get; set; }
+        public string ProjectName { get; set; }
+    }
+}
