a7fa12b3efdc8f68526d32d25bdaa08e7eb81305
muhammad-abdulloh <clevercoderr@gmail.com>
2022-12-18 11:57:41 +0500

    DOMAIN: Create ClientDTO model

---
+++ MockInterview.Domain/Models/Client/ClientDTO.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterview.Domain.Models.Client
+{
+    public class ClientDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;
+        public string Experience { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public IFormFile? Image { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
