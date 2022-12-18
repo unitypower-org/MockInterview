4f12b84cd0d00f8674ec36c4cb059f52e71e7bb0
muhammad-abdulloh <clevercoderr@gmail.com>
2022-12-18 20:11:25 +0500

    INFRASTRUCTURE: Create Client Repository

---
+++ MockInterview.Infrastructure/IRepositories/IClientRepasitory.cs
@@ -0,0 +1,14 @@
+﻿using MockInterview.Domain.Entities.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterview.Infrastructure.IRepositories
+{
+    public interface IClientRepasitory : IGenericRepository<Client>
+    {
+        Task<Client> UpdateAsync(long id);
+    }
+}
