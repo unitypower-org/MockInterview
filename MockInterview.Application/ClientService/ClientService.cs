8de638914d08b20b01673f7dbf4979cfb78ab9ae
muhammad-abdulloh <clevercoderr@gmail.com>
2022-12-18 21:41:25 +0500

    INFRASTRUCTURE: Create service and repositories and upgrade

--- MockInterview.Application/ClientService/ClientService.cs
+++ MockInterview.Application/ClientService/ClientService.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using MockInterview.Domain.Entities.Client;
+using MockInterview.Domain.Models.Client;
+using MockInterview.Infrastructure.IRepositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MockInterview.Application.ClientService
 {
-    public class ClientService
+    public class ClientService : IClientService
     {
+        private readonly IClientRepasitory _clientRepository;
+        public Task<Client> CreateAsync(ClientDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Client>> GetAllAsync(int pageSize, int pageIndex, Expression<Func<Client, bool>> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> GetAsync(Expression<Func<Client, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> UpdateAsync(Client model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> UpdateStatusAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MockInterview.Application/ClientService/IClientService.cs
+++ MockInterview.Application/ClientService/IClientService.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using MockInterview.Domain.Entities.Client;
+using MockInterview.Domain.Models.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MockInterview.Application.ClientService
 {
-    public class IClientService
+    public interface IClientService
     {
+        Task<Client> CreateAsync(ClientDTO model);
+        Task<Client> GetAsync(Expression<Func<Client, bool>> predicate);
+        Task<IEnumerable<Client>> GetAllAsync(int pageSize, int pageIndex, Expression<Func<Client, bool>> predicate = null);
+        Task<Client> UpdateAsync(Client model);
+        Task<Client> UpdateStatusAsync(long id);
     }
 }
