0b0069d412fad063ffcdb441bb3cd0e55990f80f
muhammad-abdulloh <clevercoderr@gmail.com>
2022-12-18 12:13:05 +0500

    DOMAIN: Create InterviewerDTO view model

---
+++ MockInterview.Domain/Models/Interviewer/InterviewerDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterview.Domain.Models.Interviewer
+{
+    public class InterviewerDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;
+        public string Experience { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public IFormFile? Image { get; set; }
+    }
+}
